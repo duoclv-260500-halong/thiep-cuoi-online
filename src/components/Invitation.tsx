6747244c6fb58befdba42139aad3c55df845c501
duoclv-260500-halong <duoclv.260500.halong@gmail.com>
2025-05-13 14:37:26 +0700

    update album

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './Invitation.css';
+import React from "react";
+import "./Invitation.css";
 
 const getInviteeName = () => {
   const params = new URLSearchParams(window.location.search);
-  const name = params.get('to');
-  return name ? decodeURIComponent(name) : 'Quý khách và Gia đình';
+  const name = params.get("to");
+  return name ? decodeURIComponent(name) : "Quý khách và Gia đình";
 };
 
 const getObjectName = () => {
   const params = new URLSearchParams(window.location.search);
-  const name = params.get('for');
-  const nameDecoded = decodeURIComponent(name ?? '');
-  if (nameDecoded === '1') return 'con chúng tôi';
-  if (nameDecoded === '2') return 'vợ chồng chúng tôi';
+  const name = params.get("for");
+  const nameDecoded = decodeURIComponent(name ?? "");
+  if (nameDecoded === "1") return "con chúng tôi";
+  if (nameDecoded === "2") return "vợ chồng chúng tôi";
   return "con chúng tôi";
-}
+};
 
 const Invitation: React.FC = () => {
   const invitee = getInviteeName();
@@ -22,7 +22,8 @@ const Invitation: React.FC = () => {
   return (
     <div className="invitation-section">
       <p>
-        <strong>Trân trọng kính mời {invitee}</strong> tới dự bữa cơm thân mật mừng hạnh phúc của hai {objectName}.
+        <strong>Trân trọng kính mời {invitee}</strong> <br />
+        Tới dự bữa cơm thân mật mừng hạnh phúc của hai {objectName}.
       </p>
     </div>
   );
--- src/components/WeddingAlbum.tsx
+++ src/components/WeddingAlbum.tsx
@@ -1,60 +1,84 @@
 import React from "react";
 import "./WeddingAlbum.css";
+import { Modal } from "antd";
 
+const images = [
+  //@ts-ignore
+  require("../assets/anhcuoi1.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi2.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi3.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi4.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi5.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi6.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi7.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi8.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi9.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi10.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi11.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi12.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi13.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi14.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi15.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi16.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi17.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi18.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi19.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi20.jpg"),
+  //@ts-ignore
+  require("../assets/anhcuoi21.jpg"),
+];
 const WeddingAlbum: React.FC = () => {
-  const images = [
-    //@ts-ignore
-    require("../assets/anhcuoi1.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi2.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi3.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi4.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi1.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi2.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi3.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi4.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi2.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi3.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi4.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi2.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi3.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi4.jpg"),
-    //@ts-ignore
-    require("../assets/anhcuoi5.jpg"),
-  ];
-
+  const [visible, setVisible] = React.useState(false);
+  const [currentImage, setCurrentImage] = React.useState(0);
   return (
     <div className="wedding-album">
       <h3>Album ảnh cưới</h3>
       <div className="album-grid">
         {images.map((image, index) => (
           <div key={index} className="album-item">
-            <img src={image} alt={`Wedding ${index + 1}`} />
+            <img
+              src={image}
+              alt={`Wedding ${index + 1}`}
+              onClick={() => {
+                setCurrentImage(index);
+                setVisible(true);
+              }}
+            />
           </div>
         ))}
       </div>
+      <Modal
+        open={visible}
+        onCancel={() => setVisible(false)}
+        footer={null}
+        width={"80%"}
+        closable={false}
+      >
+        <img
+          src={images[currentImage]}
+          alt={`Wedding ${currentImage + 1}`}
+          style={{ width: "100%", height: "auto" }}
+        />
+      </Modal>
     </div>
   );
 };
